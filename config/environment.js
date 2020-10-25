717d1d8b0f0eb76ac9edcc61a3caec127c060c8f
Makan <makan.edrisi@gmail.com>
2020-10-25 22:05:04 +0800

    done

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = "/ember-multi-tabs-experiment"
+    ENV.rootURL = '/ember-multi-tabs-experiment'
     // here you can enable a production-specific feature
   }
 
